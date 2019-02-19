c3bf781198567cf535678b3c0262a119866098bf
deje212 <dedi.dwianto@gmail.com>
2019-02-19 14:26:58 +0700

    Create AnonymousArray.java

---
+++ AnonymousArray.java
@@ -0,0 +1,8 @@
+public class AnonymousArray{
+      public static void printArray(String[] data){ 
+         System.out.println(data);
+      }
+      public static void main(String[] args){ 
+          printArray(new String[]{"satu","dua","tiga"}); 
+      }
+}
