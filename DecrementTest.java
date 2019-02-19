9bf494ced9e84adf433caa101cfddd31c9b16f69
deje212 <dedi.dwianto@gmail.com>
2019-02-20 06:18:17 +0700

    Update and rename IncrementTest.java to DecrementTest.java

---
+++ DecrementTest.java
@@ -0,0 +1,9 @@
+public class DecrementTest{
+  public static void main(String[] args){
+    int x = 10;
+    System.println("x-- = " + x--);
+    System.println("Setelah evaluasi, x = " + x);
+    x = 10;
+    System.println("--x = " + --x);
+    System.println("Setelah evaluasi, x = " + x);
+} }
