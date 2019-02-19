c558453cce18be79ebe013f8cd44f52ac239bf72
deje212 <dedi.dwianto@gmail.com>
2019-02-20 06:17:36 +0700

    Create IncrementTest.java

---
+++ IncrementTest.java
@@ -0,0 +1,9 @@
+public class IncrementTest{
+  public static void main(String[] args){
+    int x = 10;
+    System.println("x++ = " + x++);
+    System.println("Setelah evaluasi, x = " + x);
+    x = 10;
+    System.println("++x = " + ++x);
+    System.println("Setelah evaluasi, x = " + x);
+} }
