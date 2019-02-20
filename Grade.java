18e55187827c1f3730e1d36b06f799f8c2e4a776
deje212 <dedi.dwianto@gmail.com>
2019-02-20 16:03:03 +0700

    Update Grade.java
    using return value

--- Grade.java
+++ Grade.java
@@ -1,24 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dwianto
+ */
 public class Grade {
     public static void main(String[] args)
-    {
-        
-        Float quiz_score =new Float(1000);
+    { 
+        Float quiz_score =new Float(100);
         Float uas_score = new Float(75.5);
         Float uts_score = new Float(75.5);
-        showGrade(quiz_score,uas_score, uts_score);
+        
+        String grade = showGrade(quiz_score,uas_score, uts_score);
+       
+        System.out.println(grade);
     }
 
-static void showGrade(Float quiz_score,Float uas_score,Float uts_score){
+static String showGrade(Float quiz_score,Float uas_score,Float uts_score){
 
     Float avg;
+    String grade;
     avg=(quiz_score+uts_score+uas_score)/3;
-
-    if(avg>=90 && (avg<100)) System.out.println("Your grade A.");
-    else if((avg>=70) && (avg<90)) System.out.println("Your grade B.");
-    else if((avg>=50) && (avg<70)) System.out.println("Your grade C.");
-    else if(avg<50) System.out.println("Your grade F.");
-    else System.out.println("Invalid");
     
+    if(avg>=90 && (avg<100)) grade = "A";
+    else if((avg>=70) && (avg<90)) grade = "B";
+    else if((avg>=50) && (avg<70)) grade = "C";
+    else if(avg<50) grade = "F";
+    else grade = "invalid";
+    
+    return grade;
 
  }
 
