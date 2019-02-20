5aa8fca85b43ac90de684cc2aa3e680d593e2c94
deje212 <dedi.dwianto@gmail.com>
2019-02-20 16:04:01 +0700

    Update SwitchDay.java
    Update multiple method and return value

--- SwitchDay.java
+++ SwitchDay.java
@@ -2,9 +2,17 @@ public class SwitchDay {
     public static void main(String[] args) {
 
       int hari = 4;
-      String day;
+      int bln  = 2;
+      String day = Hari(hari);
+      String bulan = Bulan(bln);
+      System.out.println("Hari ke " + hari + " Adalah : " + day + " dan Bulan ke " + bln + " Adalah :" + bulan);
+
+    }
+    
+    public static String Hari(Integer hari) {
+        String day;
 	        
-      switch (hari) {
+        switch (hari) {
          case 1:
            day = "Senin";
            break;
@@ -30,6 +38,55 @@ public class SwitchDay {
            day = "Invalid day";
            break;
       }
-      System.out.println("Hari ke " + hari + " Adalah : " + day);
-   }
+        return day;   
+    }
+    
+    public static String Bulan(Integer bln) {
+        String bulan;
+	        
+        switch (bln) {
+         case 1:
+           bulan = "Januari";
+           break;
+         case 2:
+           bulan = "Febuari";
+	   break;
+         case 3:
+           bulan = "Maret";
+           break;
+         case 4:
+           bulan = "April";
+           break;
+         case 5:
+           bulan = "Mei";
+           break;
+         case 6:
+           bulan = "Juni";
+           break;
+         case 7:
+           bulan = "Juli";
+           break;
+         case 8:
+           bulan = "Agustus";
+           break;
+         case 9:
+           bulan = "September";
+           break;
+         case 10:
+           bulan = "Oktober";
+           break;
+         case 11:
+           bulan = "November";
+           break;
+         case 12:
+           bulan = "Desember";
+           break;
+         default: 
+           bulan = "Febuari";
+           break;
+      }
+        return bulan;   
+    }
+    
+    
 }
