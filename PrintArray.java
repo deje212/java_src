d27b9acc9d5caedc24f04ce08514ab8a4f4e20f6
deje212 <dedi.dwianto@gmail.com>
2019-02-20 11:39:49 +0700

    Create PrintArray.java

---
+++ PrintArray.java
@@ -0,0 +1,15 @@
+public class PrintArray {
+    
+     
+     public static void main(String[] arg){
+        
+         String[] ENC = {"AES","MD5","SHA1","SHA256"};
+         System.out.println("Result:");
+         int panjang = ENC.length;
+         for (int i = 0; i < panjang; ++i) {
+             System.out.println(ENC[i]);
+          }
+        
+    }
+    
+}
