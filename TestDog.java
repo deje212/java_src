12056271b543e54a49e833a988a923d6d2442352
deje212 <dedi.dwianto@gmail.com>
2019-02-22 10:00:24 +0700

    Create TestDog.java

---
+++ TestDog.java
@@ -0,0 +1,8 @@
+
+public class TestDog {
+
+   public static void main(String args[]) {
+      Animal b = new Dog();   // Animal reference but Dog object
+      b.move();   // runs the method in Dog class
+   }
+}
